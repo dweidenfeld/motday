eee3a8f5073688e412006aa3b1eba8f062cfe088
Dominik Weidenfeld <dom.weidenfeld@gmail.com>
2015-05-31 22:15:42 +0200

    performance improvement

--- motday.go
+++ motday.go
@@ -9,37 +9,49 @@ import (
 	"github.com/dweidenfeld/motday/flickr"
 )
 
+var (
+	dc chan Data
+)
+
+func init() {
+	dc = make(chan Data, 10)
+}
+
 func main() {
+	c, err := config.Load("config.json")
+	if nil != err {
+		panic(err)
+	}
+	f := flickr.New(c.Flickr.APIKey)
+	go fetchNext(c, f)
 	http.HandleFunc("/", photoHandler)
 	http.HandleFunc("/favicon.ico", http.NotFoundHandler().ServeHTTP)
 	http.ListenAndServe(":8080", nil)
 }
 
+func fetchNext(c *config.Config, f *flickr.Flickr) {
+	for {
+		motive := c.RandomMotive()
+		query := strings.Join(motive.Queries, ", ")
+
+		image, err := f.SearchRandom(query)
+		if nil != err {
+			panic(err)
+		}
+
+		dc <- Data{
+			Motive: motive,
+			Image:  image,
+		}
+	}
+}
+
 func photoHandler(w http.ResponseWriter, r *http.Request) {
-	config, err := config.Load("config.json")
-	if nil != err {
-		panic(err)
-	}
-	flickr := flickr.New(config.Flickr.APIKey)
-
-	motive := config.RandomMotive()
-	query := strings.Join(motive.Queries, ", ")
-	//query := *motive.RandomQuery()
-
-	image, err := flickr.SearchRandom(query)
-	if nil != err {
-		panic(err)
-	}
-
 	t, err := template.ParseFiles("photoTemplate.html")
 	if nil != err {
 		panic(err)
 	}
-
-	t.Execute(w, Data{
-		Motive: motive,
-		Image:  image,
-	})
+	t.Execute(w, <-dc)
 }
 
 // Data Model
